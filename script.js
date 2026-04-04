69f1f778aecbcc3e831d640a106356c633bc2a68
adm789 <adamang@tutamail.com>
2026-04-04 20:42:26 +0800

    Add files via upload
    php.css.js

---
+++ script.js
@@ -0,0 +1,21 @@
+﻿let display = document.getElementById('display');
+let currentInput = "";
+
+function press(value) {
+    currentInput += value;
+    display.innerText = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = "";
+    display.innerText = "0";
+}
+
+function calculate() {
+    try {
+        currentInput = eval(currentInput).toString();
+        display.innerText = currentInput;
+    } catch (e) {
+        display.innerText = "Error";
+    }
+}
